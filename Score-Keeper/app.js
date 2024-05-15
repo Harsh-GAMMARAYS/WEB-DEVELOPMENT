e665e3daff4e1cf1b4cb83f96937139d4eb87dac
Harsh-GAMMARAYS <gammaray.coding@gmail.com>
2024-05-16 02:06:22 +0530

    added frontend

--- Score-Keeper/app.js
+++ Score-Keeper/app.js
@@ -16,6 +16,9 @@ p1Button.addEventListener('click', function () {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('has-text-success')
+            p2Display.classList.add('has-text-danger')
+
         }
         p1Display.textContent = p1Score;
     }
@@ -26,6 +29,8 @@ p2Button.addEventListener('click', function () {
         p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
+            p2Display.classList.add('has-text-success')
+            p1Display.classList.add('has-text-danger')
         }
         p2Display.textContent = p2Score;
     }
@@ -44,6 +49,9 @@ function reset() {
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+
 
 }
 
