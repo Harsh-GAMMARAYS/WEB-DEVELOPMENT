d2a8eb92def3f28ba655951a8e4ecc4d429e5950
Harsh-GAMMARAYS <gammaray.coding@gmail.com>
2024-05-21 00:05:31 +0530

    new project started "drums"

---
+++ drum/index.js
@@ -0,0 +1,8 @@
+let numOfDrumBtn = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numOfDrumBtn; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",
+        function () {
+            alert("I got clicked!")
+        });
+}
