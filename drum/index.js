c6a51c71e25bdb12cd255efa8da7f5c3f3d55136
Harsh-GAMMARAYS <gammaray.coding@gmail.com>
2024-05-21 23:52:30 +0530

    added images

--- drum/index.js
+++ drum/index.js
@@ -3,6 +3,7 @@ let numOfDrumBtn = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numOfDrumBtn; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click",
         function () {
-            alert("I got clicked!")
+            let audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
         });
 }
