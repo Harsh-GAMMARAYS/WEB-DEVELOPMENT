278f13a161a255f93601b7965956e618c5a23d5b
Harsh-GAMMARAYS <gammaray.coding@gmail.com>
2024-05-22 20:20:17 +0530

    made a quote generator

---
+++ simpleProjects/FirstProject/index.js
@@ -0,0 +1,99 @@
+// Variables
+
+let btn = document.querySelector("#new-quote");
+let quote = document.querySelector(".quote");
+let person = document.querySelector(".person");
+
+const quotes =[
+    {
+        "quote": "Be yourself; everyone else is already taken.",
+        "author": "Oscar Wilde",
+        "profession": "Irish poet",
+        "topics": [
+            "Inspirational",
+            "Advice",
+            "Humor"
+        ]
+    },
+    {
+        "quote": "To live is the rarest thing in the world. Most people exist, that is all.",
+        "author": "Oscar Wilde",
+        "profession": "Irish poet",
+        "topics": [
+            "Philosophy",
+            "Humor"
+        ]
+    },
+    {
+        "quote": "True friends stab you in the front.",
+        "author": "Oscar Wilde",
+        "profession": "Irish poet",
+        "topics": [
+            "Philosophy",
+            "People"
+        ]
+    },
+    {
+        "quote": "Women are made to be Loved, not understood.",
+        "author": "Oscar Wilde",
+        "profession": "Irish poet",
+        "topics": [
+            "Philosophy",
+            "Humor"
+        ]
+    },
+    {
+        "quote": "Be the change that you wish to see in the world.",
+        "author": "Mahatma Gandhi",
+        "profession": "Indian leader",
+        "topics": [
+            "Inspirational",
+            "Philosophy",
+            "Advice"
+        ]
+    },
+    {
+        "quote": "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+        "author": "Mahatma Gandhi",
+        "profession": "Indian leader",
+        "topics": [
+            "Inspirational",
+            "Life",
+            "Advice"
+        ]
+    },
+    {
+        "quote": "No one can make you feel inferior without your consent.",
+        "author": "Eleanor Roosevelt",
+        "profession": "Former First Lady of the United States",
+        "topics": [
+            "Wisdom"
+        ]
+    },
+    {
+        "quote": "Great minds discuss ideas; average minds discuss events; small minds discuss people.",
+        "author": "Eleanor Roosevelt",
+        "profession": "Former First Lady of the United States",
+        "topics": [
+            "Wisdom",
+            "People"
+        ]
+    },
+    {
+        "quote": "Do what you feel in your heart to be right - for you'll be criticized anyway.",
+        "author": "Eleanor Roosevelt",
+        "profession": "Former First Lady of the United States",
+        "topics": [
+            "Wisdom",
+            "Advice"
+        ]
+    }
+];
+
+btn.addEventListener('click', function () {
+
+    let random = Math.floor(Math.random() * quotes.length);
+    quote.innerText = quotes[random].quote;
+    person.innerText = quotes[random].author;
+
+});
