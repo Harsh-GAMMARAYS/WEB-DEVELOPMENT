8add645f01c32517aa124f2cc7441e1980f039ad
Harsh-GAMMARAYS <gammaray.coding@gmail.com>
2024-06-21 17:43:50 +0530

    added error handling

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const AppError = require('./AppError');
+
+app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
     req.requestTime = Date.now();
@@ -17,12 +21,19 @@ const verifyPassword = (req, res, next) => {
     if (password === 'chickennugget') {
         next();
     }
-    res.send('NO,You need a password')
+    throw new AppError('password required', 401);
+    // res.send('NO,You need a password')
+    // res.status(401);
+    // throw new Error('Password Required!')
 }
 
 app.get('/', (req, res) => {
     res.send("HOME PAGE")
 });
+
+app.get('/error', (req, res) => {
+    chicken.fly()
+})
 
 app.get('/dogs', (req, res) => {
     res.send("WOOF")
@@ -34,6 +45,13 @@ app.get('/secret', verifyPassword, (req, res) => {
 
 app.use((req, res) => {
     res.status(404).send('NOT FOUND')
+});
+
+app.use((err, req, res, next) => {
+    console.log("**************************************")
+    console.log("***************ERROR******************")
+    console.log("**************************************")
+    next(err);
 });
 
 app.listen(3000, () => {
